59890510c2b4642ee5d705ae4f2e50fb269a77aa
DaltoGit <alvelycken@gmail.com>
2025-04-14 14:48:08 +0200

    redid controls

--- player_feet.gd
+++ player_feet.gd
@@ -4,24 +4,24 @@ const mSpeed: float=50.0
 const fSpeed: float=1.2
 const fallDistance: float=50.0
 
-var feet=[null, null]
-var currentFoot: int=0
+var lFoot=null
+var rFoot=null
 
 func _ready() -> void:
-	feet=[get_node("left"), get_node("right")]
+	lFoot=get_node("left")
+	rFoot=get_node("right")
 	return
 
 func _process(delta):
-	var movement: Vector2=Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if Input.is_action_just_pressed("ui_accept"):
-		currentFoot=int(not currentFoot)
-	feet[0].velocity=Vector2(0,0)
-	feet[1].velocity=Vector2(0,0)
-	feet[currentFoot].velocity+=movement*mSpeed
-	var feetposvector: Vector2=feet[0].position-feet[1].position
+	var lMovement: Vector2=Input.get_vector("lLeft", "lRight", "lUp", "lDown")
+	var rMovement: Vector2=Input.get_vector("rLeft", "rRight", "rUp", "rDown")
+	
+	var feetposvector: Vector2=lFoot.position-rFoot.position
 	if feetposvector.length_squared()>fallDistance**2: print("you lost!")
-	feet[0].velocity+=feetposvector*fSpeed
-	feet[1].velocity+=(-feetposvector)*fSpeed
-	feet[0].move_and_slide()
-	feet[1].move_and_slide()
+	
+	lFoot.velocity=feetposvector*fSpeed+lMovement*mSpeed
+	rFoot.velocity=(-feetposvector)*fSpeed+rMovement*mSpeed
+	
+	lFoot.move_and_slide()
+	rFoot.move_and_slide()
 	return
